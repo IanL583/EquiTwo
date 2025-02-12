8f825cd556af78b46dfd71049ee499910c3db33a
Ian Lin <tingweilin168@gmail.com>
2025-02-12 14:49:20 +1100

    added structure for an object oriented approach

---
+++ backend/equity.py
@@ -0,0 +1,5 @@
+# equity logic
+
+# calculating the equity with the inputs of two hands and an intial empty board (which can change for post-flop calculation)
+def calculate_equity(hand_one, hand_two, board=None):
+    return 100.0
