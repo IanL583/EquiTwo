ed62966ca5ecfcafcef72ed7f0f1538202f9d3b1
Ian Lin <tingweilin168@gmail.com>
2025-02-16 20:37:41 +1100

    reduced redudant code in equity calculation

--- backend/equity.py
+++ backend/equity.py
@@ -18,55 +18,28 @@ class Equity:
         seen_cards =  {str(card) for card in self.hero.cards + self.villain.cards + self.board.cards}
         return [Card(card) for card in deck - seen_cards]
 
-    # need to use combinations of 5 hand boards for equity streets and also potential outs
-    # calculate pre-flop equity
-    def calculate_preflop_equity(self):
-        # counters for hero or villain winning, tying, and also board configurations
+    # calculate the equity of two hands based on the board state
+    def calculate_equity(self):
+        # counters for winning scenarios for the hero or the villain or ties with different potential boards
         hero_wins = 0
         villain_wins = 0
         ties = 0
         boards = 0
-        # loop through all the different 5 card board combinations
-        for board in combinations(self.deck, 5):
-            # increment the board combinations and evaluate the made hands with the board
+        # get cards needed to complete the board from either preflop, flop, turn or river
+        remaining_cards = 5 - len(self.board.cards)
+        for board in combinations(self.deck, remaining_cards):
             boards += 1
-            final_board = Board(''.join(map(str, board)))
-            winner = self.evaluate_hand_ranking(self.hero, self.villain, final_board)
-            # now evaluate the winning or tying scenarios and increment those spots
-            if winner == 1:
-                hero_wins += 1
-            elif winner == -1:
-                villain_wins += 1
-            else:
-                ties += 1
-        # return the winning and tying scenarios to the amount of different boards to calculate the equity
-        return {
-            "Hero Equity": hero_wins / boards * 100,
-            "Villain Equity": villain_wins / boards * 100,
-            "Tie Equity": ties / boards * 100
-        }
-
-    # calculate post-flop equity
-    def calcaulte_postflop_equity(self):
-        # counters for hero or villain winning, tying, and also board configurations
-        hero_wins = 0
-        villain_wins = 0
-        ties = 0
-        boards = 0
-        # loop through all the different board combinations of outs for either flop, turn or river
-        for board in combinations(self.deck, 5 - len(self.board.cards)):
-            # increment the board combinations and evaluate the made hands with the existing board
-            boards += 1
+            # create final board using outs
             final_board = Board(str(self.board) + ''.join(map(str, board)))
+            # get the winner of each board and increment the counters
             winner = self.evaluate_hand_ranking(self.hero, self.villain, final_board)
-            # now evaluate the winning or tying scenarios and increment those spots
             if winner == 1:
                 hero_wins += 1
             elif winner == -1:
                 villain_wins += 1
             else:
                 ties += 1
-        # return the winning and tying scenarios to the amount of different boards to calculate the equity
+        # return the equity percentages
         return {
             "Hero Equity": hero_wins / boards * 100,
             "Villain Equity": villain_wins / boards * 100,
@@ -74,18 +47,9 @@ class Equity:
         }
 
     # evaluate which hand is stronger with hand rankings
-    def evaluate_hand_ranking(self):
+    def evaluate_hand_ranking(self, hero: Hand, villain: Hand, board: Board):
         # need poker hand ranking boolean evaluations
         return 1
-
-    # return the final equity based on the type of hand spot
-    def calculate_final_equity(self):
-        # calculation for preflop case
-        if len(self.board.cards) == 0:
-            return self.calculate_preflop_equity()
-        else:
-            # postflop case if there is an existing board
-            return self.calcaulte_postflop_equity()
 
 # test outputs for debugging
 hand_one = Hand("AsAd")
