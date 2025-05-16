397bdd7af400f5beebd87b20c6b2cb30f170a128
Ian Lin <tingweilin168@gmail.com>
2025-05-16 23:27:17 +1000

    planned precomputed equity file

---
+++ precomputation.py
@@ -0,0 +1,14 @@
+import json
+from itertools import product
+from card import Card
+from hand import Hand
+from board import Board
+from equity import Equity
+
+# helper function to convert notation to cards
+
+# generate all preflop hand combinations
+
+# calculate all the equities for headsup matchups
+
+# save these computations into a json file to utilise in the frontend
